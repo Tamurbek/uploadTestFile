bc12b4fcec80b0fe59444ce3e09dcb953baa4336
Tamurbek <temuryoldoshev10@gmail.com>
2023-05-03 12:57:43 +0500

    push1

---
+++ src/main/java/com/example/demoupload/model/FileDB.java
@@ -0,0 +1,26 @@
+package com.example.demoupload.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "files")
+public class FileDB {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    private byte[] data;
+}
---
+++ src/main/java/com/example/demoupload/repository/FileDBRepository.java
@@ -0,0 +1,10 @@
+package com.example.demoupload.repository;
+
+import com.example.demoupload.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
+}
